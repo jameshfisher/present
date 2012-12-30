01bc8b3e873ac86e52626f6f6ca0013a4af2759c
James Fisher <jameshfisher@gmail.com>
2012-12-30 00:14:50 +0000

    more puzzles; music; move counter; piece highlighting

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -6,4 +6,5 @@ import Import
 getHomeR :: Handler RepHtml
 getHomeR = defaultLayout $ do
     setTitle "Puzzle Present"
+    addScriptRemote "/static/js/jaysalvat-buzz-05c96cc/buzz.js"
     $(widgetFile "homepage")
